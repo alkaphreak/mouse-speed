fdf9d7406adb24790f01708754a54e1fdfff98a4
alkaphreak <electron.libre@gmail.com>
2016-08-26 11:40:40 +0200

    Reducing the speed from 60% to 50%

---
+++ mouse-speed-start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+notify-send -i input-mouse "Adjusting mouse speed"
+#/home/alkaphreak/Projects/mouse-speed/usr/bin/mouse-speed -s60
+/home/alkaphreak/Projects/mouse-speed/usr/bin/mouse-speed -s50
